5437c0bc8e5313ba578c3fa18912bdde8f050acf
Bryan-Campuzano <bryam11122113@gmail.com>
2024-06-21 18:37:01 -0500

    commit de actualizacion: mitad de proyecto

--- ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7a30ccce3b4e4103213f942e012c16e9ac9d5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e4285e107365f3a3541d7c084d027fd6e99df9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
